ad61eb999a8e606b17dd491f865a52643448ca2d
ninjafrostpn <charles.turvey@rocketmail.com>
2015-06-21 13:42:49 +0100

    Create SOLUTION.py

---
+++ problem3/SOLUTION.py
@@ -0,0 +1,4 @@
+name = input("Hey, I ain't seen you 'angin' round 'ere before. Wha's yer name?")
+age = input("So you're %s, eh? How old are ya anyways? Too young to be in a place like this, I'd wager." % name)
+print("%s is %s years old" % (name, age))
+print("Yeah, boss man says yer too young, so gertcha!" % name)
